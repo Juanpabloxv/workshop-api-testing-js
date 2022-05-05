f9c1f40f06d1ec7cf12ad8051a4d104118f1cf0e
Juanpabloxv <juanpabloxv@hotmail.es>
2022-05-05 00:26:00 -0500

    Trabajo completo

---
+++ test/GithubApi.Authentication.test.js
@@ -0,0 +1,31 @@
+/* eslint-disable import/prefer-default-export */
+const agent = require('superagent');
+const statusCode = require('http-status-codes');
+const { expect } = require('chai');
+
+const urlBase = 'https://api.github.com';
+const githubUserName = 'Juanpabloxv';
+const repository = 'workshop-api-testing-js';
+
+// const ACCESS_TOKEN = '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';
+
+describe('Github Api Test', () => {
+  describe('Authentication', () => {
+    it('Via OAuth2 Tokens by Header', async () => {
+      const response = await agent.get(`${urlBase}/repos/${githubUserName}/${repository}`)
+        .auth('token', process.env.ACCESS_TOKEN)
+        .set('User-Agent', 'agent');
+
+      expect(response.status).to.equal(statusCode.OK);
+      expect(response.body.description).equal('This is a Workshop about Api Testing in JavaScript');
+    });
+
+    it('Via OAuth2 Tokens by parameter', () => agent.get(`${urlBase}/repos/${githubUserName}/${repository}`)
+      .query(`access_token=${process.env.ACCESS_TOKEN}`)
+      .set('User-Agent', 'agent')
+      .then((response) => {
+        expect(response.status).to.equal(statusCode.OK);
+        expect(response.body.description).equal('This is a Workshop about Api Testing in JavaScript');
+      }));
+  });
+});
